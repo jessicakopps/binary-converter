4e8ed2a172761ab0e4422a392ad943de513a1e7b
Jessica Kopps <22087289+jessicakopps@users.noreply.github.com>
2022-04-27 23:59:21 -0300

    Create Binary.java

---
+++ Binary.java
@@ -0,0 +1,45 @@
+    public class Binary {
+    
+    public static int binarioParaDecimal(int[] vetor){
+        int index = 0;
+        int binarioParaDecimal = 0;
+        for(int i = vetor.length -1; i >= 0; i--){
+            if(vetor[i] == 1){
+                binarioParaDecimal += Math.pow(2,index);
+            }
+            index++;
+        }
+        return binarioParaDecimal;
+    }
+
+    public static String decimalParaBinario(int valor){
+        int potencia = 0;
+        double resultado = 0.0;
+
+        //verificar limite da potencia
+        while (true) {
+            if (resultado > valor || Math.pow(2, (potencia + 1)) > valor) {
+                break;
+            } else {
+                resultado = Math.pow(2, potencia);
+                potencia++;
+            }
+        }
+
+        //realiza a conversao de decimal para binario
+        double resto = valor;
+        String binario = "";
+
+        for (int i = potencia; i >= 0; i--) {
+            double r = Math.pow(2, i);
+
+            if (r <= resto) {
+                binario += "1";
+                resto -= r;
+            } else {
+                binario += "0";
+            }
+        }
+        return binario;
+    }
+}
